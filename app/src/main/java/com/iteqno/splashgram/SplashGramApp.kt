b0ddc67d218a9a1b4850804048c8f1746582a08d
Khofidin <offiedz@gmail.com>
2020-10-05 19:19:40 +0700

    Add experimental annotation

--- app/src/main/java/com/iteqno/splashgram/SplashGramApp.kt
+++ app/src/main/java/com/iteqno/splashgram/SplashGramApp.kt
@@ -6,11 +6,15 @@ import com.iteqno.splashgram.core.di.networkModule
 import com.iteqno.splashgram.core.di.repositoryModule
 import com.iteqno.splashgram.di.useCaseModule
 import com.iteqno.splashgram.di.viewModelModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 class SplashGramApp : Application() {
 
     override fun onCreate() {
